2cc322b0fa1f43a772ed8af0efdbe815a70418cd
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-20 16:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let text=document.querySelector("#status")
+let h1=document.querySelector("#h")
+
+document.querySelector("#enterBtn").addEventListener('click',()=>{
+	//h1.innerText=" "
+text.innerHTML="<h1>Entered Metaverse</h1>"
+	
+})
+
